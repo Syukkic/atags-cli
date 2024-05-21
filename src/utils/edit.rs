82fb197f5a1d645dc2ac5b192187b118f548e0f3
Yuki.N <Syukkic@gmail.com>
2024-05-21 11:19:49 +0800

    Don't use subcommand

--- src/utils/edit.rs
+++ src/utils/edit.rs
@@ -1,44 +1,36 @@
 use audiotags::Tag;
 
-use crate::PathBuf;
+use crate::Commands;
 
-pub fn set(
-    file: &PathBuf,
-    title: Option<String>,
-    artist: Option<String>,
-    album_title: Option<String>,
-    genre: Option<String>,
-    composer: Option<String>,
-    track_number: Option<u16>,
-) {
+pub fn set(args: &Commands) {
     let mut tag = Tag::new()
-        .read_from_path(file)
+        .read_from_path(&args.name)
         .expect("Audio File Not Found");
 
-    if let Some(title) = title {
+    if let Some(title) = &args.title {
         tag.set_title(title.as_str())
     }
 
-    if let Some(album_title) = album_title {
+    if let Some(album_title) = &args.album_title {
         tag.set_album_title(album_title.as_str())
     }
 
-    if let Some(artist) = artist {
+    if let Some(artist) = &args.artist {
         tag.set_artist(artist.as_str())
     }
 
-    if let Some(genre) = genre {
+    if let Some(genre) = &args.genre {
         tag.set_genre(genre.as_str())
     }
 
-    if let Some(composer) = composer {
-        tag.set_composer(composer)
+    if let Some(composer) = &args.composer {
+        tag.set_composer(composer.to_string())
     }
 
-    if let Some(track_number) = track_number {
+    if let Some(track_number) = args.track_number {
         tag.set_track_number(track_number)
     }
 
-    tag.write_to_path(file.as_os_str().to_str().unwrap())
+    tag.write_to_path(args.name.to_str().unwrap())
         .expect("Fail to save");
 }
